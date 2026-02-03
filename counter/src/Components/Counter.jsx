f8b581da46cda0e5458e068a8552cf7ac1afdfe5
Pratham_Pundir <124234938+Prathamm88@users.noreply.github.com>
2026-02-03 23:50:37 +0530

    made a click counter

--- counter/src/Components/Counter.jsx
+++ counter/src/Components/Counter.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Counter.css'
 const Counter = () => {
+  const [count, setCount]= useState(0);
   return (
     <div className='counter-container'>
-      <p id='para'>you have clicked 0 times </p>
-      <button id='button'>click me </button>
+      <p id='para'>you have clicked {count } times </p>
+      <button id='btn' on onClick={()=>{setCount(count+1)}}>click me </button>
     </div>
   )
 } 
