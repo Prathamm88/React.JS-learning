c7ba3cd3baf222fce624fa66a3b7b0c2d0402c64
Pratham_Pundir <124234938+Prathamm88@users.noreply.github.com>
2026-02-03 23:18:11 +0530

    practicing

---
+++ counter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Counter from './Components/Counter'
+function App() {
+  
+
+  return (
+    <div>
+        <Counter />
+      </div>
+   
+
+  
+      
+  )
+}
+
+export default App
---
+++ counter/src/Components/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Counter.css'
+const Counter = () => {
+  return (
+    <div className='counter-container'>
+      <p id='para'>you have clicked 0 times </p>
+      <button id='button'>click me </button>
+    </div>
+  )
+} 
+
+export default Counter 
