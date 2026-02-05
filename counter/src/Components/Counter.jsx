59ccbb46aaaf4d0f2fe65c108613e8d141a657c9
Pratham_Pundir <124234938+Prathamm88@users.noreply.github.com>
2026-02-05 17:41:05 +0530

    useEffect learned

--- counter/src/Components/Counter.jsx
+++ counter/src/Components/Counter.jsx
@@ -1,13 +1,54 @@
-import React, { useState } from 'react'
+import React, { useEffect, useEffectEvent, useState } from 'react'
 import './Counter.css'
 const Counter = () => {
-  const [count, setCount]= useState(0);
-  return (
-    <div className='counter-container'>
-      <p id='para'>you have clicked {count } times </p>
-      <button id='btn' on onClick={()=>{setCount(count+1)}}>click me </button>
-    </div>
-  )
-} 
+   const [count, setCount]= useState(0);
+//   return (
+//     <div className='counter-container'>
+//       <p id='para'>you have clicked {count } times </p>
+//       <button id='btn' on onClick={()=>{setCount(count+1)}}>click me </button>
+//     </div>
+//   
 
+
+// this is the first variation of use effect hooks, there are total of 3 variations of it 
+// in first it will run after render
+// in second First Render + When Dependency Changes
+// third one runs on every render a
+// useEffect(()=>{
+//   alert("i will run on only first render ") // first variation
+
+// },[])
+
+// function handleClick(){
+//   setCount(count+1);
+// }
+
+// second variation
+useEffect(()=>{
+  alert("i will run only when count changes  ") 
+
+},[count])
+
+// third variation
+// useEffect(()=>{
+//   alert("i will run everytime  ") 
+
+// })
+
+function handleClick(){
+  setCount(count+1);
+}
+
+return(
+
+<div>
+  <button onClick={handleClick}>
+    click me !
+  </button>
+  <br />
+  count is : {count}
+</div>
+
+)
+}
 export default Counter 
